35d3a49074994224cbad87fb45bf27d5a1774a5d
prajwal-gadhave-selldo <prajwal.ghadhave@sell.do>
2025-03-20 13:20:41 +0530

    feat: Added Precision Control and Improved Ux

--- src/components/ClothingSuggestionForm.jsx
+++ src/components/ClothingSuggestionForm.jsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import generateClothingSuggestions from '@/lib/openai';
-import { useState, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
-import { Button } from './ui/button';
-import { TiHeart } from 'react-icons/ti';
-import { useToast } from '@/components/ui/use-toast';
-import { useForm } from 'react-hook-form';
-import { CgSpinner } from 'react-icons/cg';
-import LoadingFav from './LoadingFav';
+import generateClothingSuggestions from "@/lib/openai";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { Button } from "./ui/button";
+import { TiHeart } from "react-icons/ti";
+import { useToast } from "@/components/ui/use-toast";
+import { useForm } from "react-hook-form";
+import { CgSpinner } from "react-icons/cg";
+import LoadingFav from "./LoadingFav";
 
 const ClothingSuggestionForm = ({ clothes, weather }) => {
-  const [eventType, setEventType] = useState('');
-  const [mood, setMood] = useState('');
-  const [suggestions, setSuggestions] = useState('');
+  const [eventType, setEventType] = useState("");
+  const [mood, setMood] = useState("");
+  const [suggestions, setSuggestions] = useState("");
   const [outfitsDisplay, setOutfitsDisplay] = useState(null);
   const [loadingSuggestions, setLoadingSuggestions] = useState(false);
   const [loadingRandomize, setLoadingRandomize] = useState(false);
@@ -25,9 +25,9 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
     }
   }, [suggestions]);
 
-  const weatherTemp = weather?.current?.temp_c || '';
-  const weatherCond = weather?.current?.condition?.text || '';
-  const wind = weather?.current?.wind_kph || '';
+  const weatherTemp = weather?.current?.temp_c || "";
+  const weatherCond = weather?.current?.condition?.text || "";
+  const wind = weather?.current?.wind_kph || "";
   const { data: session } = useSession();
   const email = session && session.user.email;
   const { toast } = useToast();
@@ -39,11 +39,11 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
     formState: { errors },
   } = useForm();
 
-  const event = watch('eventType');
-  const feeling = watch('mood');
+  const event = watch("eventType");
+  const feeling = watch("mood");
 
   const clothingDescriptions = clothes.map((item) => {
-    let description = `${item.colors.join(' with ')} ${item.category}`;
+    let description = `${item.colors.join(" with ")} ${item.category}`;
     if (item.pattern) {
       description += ` ${item.pattern}`;
     }
@@ -66,7 +66,7 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
     );
 
     setSuggestions(suggestions);
-    console.log(suggestions)
+    console.log(suggestions);
     setResponseLoading(false);
     setLoadingSuggestions(false);
   };
@@ -97,26 +97,26 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
 
   const handleSaveLikedOutfit = async (itemIds, email) => {
     try {
-      const response = await fetch('/api/saveLikedOutfit?email=' + email + '', {
-        method: 'POST',
+      const response = await fetch("/api/saveLikedOutfit?email=" + email + "", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ itemIds }),
       });
       await response.json();
 
       toast({
-        title: 'Item saved to Favorites',
-        description: 'Your item has been saved successfully.',
-        variant: 'success',
+        title: "Item saved to Favorites",
+        description: "Your item has been saved successfully.",
+        variant: "success",
         duration: 3000,
       });
     } catch (error) {
       toast({
-        title: 'Failed to save the item',
-        description: 'Please try again later.',
-        variant: 'destructive',
+        title: "Failed to save the item",
+        description: "Please try again later.",
+        variant: "destructive",
         duration: 3000,
       });
       console.error(error);
@@ -125,7 +125,7 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
 
   const getUserOutfit = async () => {
     const response = await fetch(`/api/findItemsUser?email=${email}`, {
-      cache: 'no-store',
+      cache: "no-store",
     });
     const userItems = await response.json();
 
@@ -141,14 +141,100 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
           );
         });
 
+        const accuracyMatch = outfitText.match(
+          /Accuracy Rating:\s*(\d+)%\s*\((.*?)\)/
+        );
+        const accuracy = accuracyMatch ? parseInt(accuracyMatch[1], 10) : 0;
+        const accuracyDescription = accuracyMatch
+          ? accuracyMatch[2].trim()
+          : "";
+
         return {
-          description: outfitText.replace(/\(([\w\d]{6})\)/g, '').trim(),
+          description: outfitText.replace(/\(([\w\d]{6})\)/g, "").trim(),
           items: outfitItems,
+          accuracy: accuracy,
+          accuracy_description: accuracyDescription,
         };
       });
+      
+      // return (
+      //   <div className="flex gap-5 mt-5 max-w-3xl mx-auto flex-wrap">
+      //     {groupedOutfits.length > 0 &&
+      //       groupedOutfits.map((outfit, index) => {
+      //         const firstQuoteIndex = outfit.description.indexOf('"') + 1;
+      //         const lastQuoteIndex = outfit.description.lastIndexOf('"');
+      //         const outfitPrefix = outfit.description.substring(
+      //           0,
+      //           firstQuoteIndex
+      //         );
+      //         const outfitName = outfit.description.substring(
+      //           firstQuoteIndex,
+      //           lastQuoteIndex + 1
+      //         );
+      //         const displayInformation = outfitPrefix + outfitName;
+
+      //         return (
+      //           <div key={index} className="w-full mt-5 relative">
+      //             {/* Outfit Name */}
+      //             <p className="text-xl font-semibold text-center">
+      //               {displayInformation}
+      //             </p>
+
+      //             {/* Accuracy Rating */}
+      //             <p className="text-lg text-gray-600 text-center mt-1">
+      //               Accuracy:{" "}
+      //               <span className="font-semibold">{outfit.accuracy}%</span>
+      //             </p>
+
+      //             {/* Accuracy Description */}
+      //             {outfit.accuracy_description && (
+      //               <p className="text-sm text-gray-500 text-center mt-1">
+      //                 {outfit.accuracy_description}
+      //               </p>
+      //             )}
+
+      //             {/* Like Button */}
+      //             <button
+      //               onClick={() =>
+      //                 handleLikeOutfit(
+      //                   index,
+      //                   outfit.items.filter((item) => item),
+      //                   email
+      //                 )
+      //               }
+      //               className="absolute top-10 right-2 p-2 z-[8] rounded-md"
+      //             >
+      //               <TiHeart className="w-8 h-8 hover:text-red-500 transition-all duration-300 ease-in" />
+      //             </button>
+
+      //             {/* Outfit Items Grid */}
+      //             <div className="grid grid-cols-2 justify-self-center gap-5 bg-secondary rounded-xl p-5">
+      //               {outfit.items
+      //                 .filter((item) => item)
+      //                 .map((item, itemIndex) => (
+      //                   <div
+      //                     key={item._id + itemIndex}
+      //                     className="relative flex items-center justify-center w-full h-full"
+      //                     style={{
+      //                       transform: `rotate(${item.rotationDegree}deg)`,
+      //                     }}
+      //                   >
+      //                     <img
+      //                       src={item.photoUrl}
+      //                       alt={item.description}
+      //                       className="object-cover h-40 w-40"
+      //                     />
+      //                   </div>
+      //                 ))}
+      //             </div>
+      //           </div>
+      //         );
+      //       })}
+      //   </div>
+      // );
 
       return (
-        <div className="flex flex-col -gap-5 mt-5 max-w-3xl mx-auto">
+        <div className="flex flex-row flex-wrap gap-5 mt-5 max-w-6xl mx-auto justify-center">
           {groupedOutfits.length > 0 &&
             groupedOutfits.map((outfit, index) => {
               const firstQuoteIndex = outfit.description.indexOf('"') + 1;
@@ -157,47 +243,64 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
                 0,
                 firstQuoteIndex
               );
-
               const outfitName = outfit.description.substring(
                 firstQuoteIndex,
                 lastQuoteIndex + 1
               );
               const displayInformation = outfitPrefix + outfitName;
+
               return (
-                <div key={index} className="w-full mt-5 relative">
-                  <p className="text-xl font-semibold text-center">
+                <div
+                  key={index}
+                  className="flex-1 min-w-[320px] max-w-[350px] bg-secondary rounded-xl p-5 shadow-md relative"
+                >
+                  {/* Outfit Title - Added back */}
+                  <p className="text-lg font-bold text-center">
                     {displayInformation}
                   </p>
-                  <button
-                    onClick={() =>
-                      handleLikeOutfit(
-                        index,
-                        outfit.items.filter((item) => item),
-                        email
-                      )
-                    }
-                    className="absolute top-10 right-2 p-2 z-[8] rounded-md"
-                  >
-                    <TiHeart
-                      className={`w-8 h-8 hover:text-red-500 transition-all duration-300 ease-in `}
-                    />
-                  </button>
 
-                  <div className="grid grid-cols-2 justify-self-center -gap-5 bg-secondary rounded-xl p-5">
+                  {/* Like Button */}
+                  <div className="flex justify-end">
+                    <button
+                      onClick={() =>
+                        handleLikeOutfit(
+                          index,
+                          outfit.items.filter((item) => item),
+                          email
+                        )
+                      }
+                      className="p-2 z-[8] rounded-md"
+                    >
+                      <TiHeart className="w-7 h-7 hover:text-red-500 transition-all duration-300 ease-in" />
+                    </button>
+                  </div>
+
+                  {/* Accuracy Rating */}
+                  <p className="text-sm text-gray-600 mt-1 text-center">
+                    Accuracy:{" "}
+                    <span className="font-semibold">{outfit.accuracy}%</span>
+                  </p>
+
+                  {/* Accuracy Description */}
+                  {outfit.accuracy_description && (
+                    <p className="text-xs text-gray-500 mt-1 text-center">
+                      {outfit.accuracy_description}
+                    </p>
+                  )}
+
+                  {/* Outfit Items Grid - Fixed Layout */}
+                  <div className="grid grid-cols-2 gap-3 mt-3">
                     {outfit.items
                       .filter((item) => item)
                       .map((item, itemIndex) => (
                         <div
                           key={item._id + itemIndex}
-                          className="relative flex items-center justify-center w-full h-full"
-                          style={{
-                            transform: `rotate(${item.rotationDegree}deg)`,
-                          }}
+                          className="relative flex items-center justify-center w-full"
                         >
                           <img
                             src={item.photoUrl}
                             alt={item.description}
-                            className="object-cover h-40 w-40"
+                            className="object-cover h-32 w-32 rounded-lg"
                           />
                         </div>
                       ))}
@@ -209,6 +312,7 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
       );
     }
   };
+
   return (
     <div className="mt-5">
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
@@ -218,7 +322,7 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
             <input
               type="text"
               autoComplete="off"
-              {...register('eventType', { required: true })}
+              {...register("eventType", { required: true })}
               className="bg-transparent h-4 w-44 md:w-60 border-b border-foreground focus:outline-none mx-2"
             />
             {errors.eventType && (
@@ -239,7 +343,7 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
             <input
               type="text"
               autoComplete="off"
-              {...register('mood', { required: true })}
+              {...register("mood", { required: true })}
               className="bg-transparent h-4 w-48 md:w-60 border-b border-foreground focus:outline-none mx-2"
             />
             {errors.mood && (
@@ -260,8 +364,8 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
           disabled={loadingRandomize || loadingSuggestions}
           className={`text-xl font-semibold mt-5 max-w-xl w-full mx-auto ${
             loadingRandomize || loadingSuggestions === true
-              ? 'opacity-50 cursor-not-allowed'
-              : ''
+              ? "opacity-50 cursor-not-allowed"
+              : ""
           }`}
         >
           {loadingSuggestions ? (
@@ -269,7 +373,7 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
               <CgSpinner className="animate-spin w-8 h-8" />
             </div>
           ) : (
-            'Get Suggestions'
+            "Get Suggestions"
           )}
         </Button>
       </form>
@@ -285,8 +389,8 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
           disabled={loadingRandomize || loadingSuggestions}
           className={`text-xl font-semibold mt-5 max-w-xl w-full mx-auto ${
             loadingRandomize || loadingSuggestions === true
-              ? 'opacity-50 cursor-not-allowed'
-              : ''
+              ? "opacity-50 cursor-not-allowed"
+              : ""
           }`}
         >
           {loadingRandomize ? (
@@ -294,7 +398,7 @@ const ClothingSuggestionForm = ({ clothes, weather }) => {
               <CgSpinner className="animate-spin w-8 h-8" />
             </div>
           ) : (
-            'Randomize'
+            "Randomize"
           )}
         </Button>
       </div>
--- src/lib/openai.js
+++ src/lib/openai.js
@@ -1,7 +1,6 @@
-'use server';
+"use server";
 
 import OpenAI from "openai";
-
 
 const openaiApiKey = process.env.OPEN_AI_API_KEY;
 
@@ -10,33 +9,60 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true,
 });
 
- const generateClothingSuggestions = async (
-   event,
-   feeling,
-   weatherTemp,
-   weatherCond,
-   clothingDescriptions,
-   wind,
-   includeEventAndMood
- ) => {
-   
-   const prompt = `You are a fashion industry expert. Given the following clothing items "${clothingDescriptions}", construct 3 different suitable and complete outfit looks for a person going to ${
-     includeEventAndMood ? event : ""
-   }, feeling ${
-     includeEventAndMood ? feeling : ""
-   }. Each outfit should include one top (like a shirt or blouse), one bottom (like pants or a skirt), and a pair of shoes. Additional accessories or layers can be suggested based on the weather conditions: ${weatherCond}, wind speed of ${wind} km/h, and temperature of ${weatherTemp}°C. Cold temperatures range from -20° to 0°, cooler weather ranges from 0° to 20°, and warm weather ranges from 20° to 40°. Present the outfits in order from top to bottom and list the exact product ids after each outfit description, ensure that there is no space before and after each outfit. Remember to use only the provided clothing items and do not invent new items or colors. Additionally give a catchy name for each outfit with exactly 3 words.`;
+const generateClothingSuggestions = async (
+  event,
+  feeling,
+  weatherTemp,
+  weatherCond,
+  clothingDescriptions,
+  wind,
+  includeEventAndMood
+) => {
+  //  const prompt = `You are a fashion industry expert. Given the following clothing items "${clothingDescriptions}", construct 3 different suitable and complete outfit looks for a person going to ${
+  //    includeEventAndMood ? event : ""
+  //  }, feeling ${
+  //    includeEventAndMood ? feeling : ""
+  //  }. Each outfit should include one top (like a shirt or blouse), one bottom (like pants or a skirt), and a pair of shoes. Additional accessories or layers can be suggested based on the weather conditions: ${weatherCond}, wind speed of ${wind} km/h, and temperature of ${weatherTemp}°C. Cold temperatures range from -20° to 0°, cooler weather ranges from 0° to 20°, and warm weather ranges from 20° to 40°. Present the outfits in order from top to bottom and list the exact product ids after each outfit description, ensure that there is no space before and after each outfit. Remember to use only the provided clothing items and do not invent new items or colors. Additionally give a catchy name for each outfit with exactly 3 words.`;
 
-   const response = await openai.completions.create({
-     model: "gpt-3.5-turbo-instruct",
-     prompt,
-     max_tokens: 1000,
-     temperature: 0.1,
-   });
+  const prompt = `You are a fashion industry expert. Given the following clothing items "${clothingDescriptions}", construct 3 different suitable and complete outfit looks for a person going to ${
+    includeEventAndMood ? event : ""
+  }, feeling ${
+    includeEventAndMood ? feeling : ""
+  }. Each outfit should include one top (like a shirt or blouse), one bottom (like pants or a skirt), and a pair of shoes. Additional accessories or layers can be suggested based on the weather conditions: ${weatherCond}, wind speed of ${wind} km/h, and temperature of ${weatherTemp}°C. Cold temperatures range from -20° to 0°, cooler weather ranges from 0° to 20°, and warm weather ranges from 20° to 40°. Each outfit should be presented in order from top to bottom, followed by the exact product IDs with no spaces before or after each outfit. Ensure that all items used come from the provided list, without inventing new items or colors. Additionally, assign a catchy three-word name to each outfit. Finally, provide an accuracy rating with name (Accuracy Rating) for each outfit, representing how well it fits the given weather, event, and mood, on a scale of 0 to 100% (rating should be in different numbers not like round figures (eg: 72%, 85%, etc)) in format like
+    Accuracy Rating: 70% (description)
+    
+    Entire Example:
+    Outfit 1: "Beach Breeze"
+    Top: aliceblue collaredshirt (21c909)
+    Bottom: white cargopants (0e503d)
+    Shoes: white platformshoes (23a43c)
+    Accessories: None
+    Accuracy Rating: 90% (perfect for warm weather and beach activities)
 
-   return response.choices[0].text;
- };
+    Outfit 2: "Casual Chic"
+    Top: black collaredshirt (3de69e)
+    Bottom: brown cargopants (6c24ac)
+    Shoes: black platformshoes (e77de5)
+    Accessories: black jeans (a1c679)
+    Accuracy Rating: 80% (suitable for cooler weather and casual outings)
+
+    Outfit 3: "Semi-Formal Fun"
+    Top: navajowhite longsleeved (de98e8)
+    Bottom: black jeans (dd9321)
+    Shoes: coralpink collaredshirt (68de65)
+    Accessories: darkbrown collaredshirt (65695e)
+    Accuracy Rating: 70% (may be too warm for hot weather, but suitable for semi-formal events)
+    .`;
+
+  const response = await openai.completions.create({
+    model: "gpt-3.5-turbo-instruct",
+    prompt,
+    max_tokens: 1000,
+    temperature: 0.1,
+  });
+
+  console.log(response.choices[0].text);
+  return response.choices[0].text;
+};
 
 export default generateClothingSuggestions;
-
-
-
